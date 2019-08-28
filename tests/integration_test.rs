8967aff67c1f0316365164720e9d6d47d9f57c3a
Julian Perelli <jperelli@gmail.com>
2019-08-28 19:14:04 +0200

    fix tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -10,11 +10,13 @@ fn count_ok() {
 #[test]
 fn get_public_transports() {
     let parser = Parser::new("tests/test.pbf", 2);
-    let pts = parser.get_public_transports(1500_f64);
-    let mut pts_iter = pts.iter();
-    // TODO: fix, this might not be in order!
-    let first = pts_iter.next().unwrap();
-    assert_eq!(first.id, 85965);
-    let second = pts_iter.next().unwrap();
-    assert_eq!(second.id, 2030162);
+    let ptsvec = parser.get_public_transports(1500_f64);
+    let mut pts = ptsvec.iter().collect::<Vec<_>>();
+    pts.sort_by(|a, b| a.id.cmp(&b.id));
+    assert_eq!(pts[0].id, 85965);
+    assert_eq!(pts[0].tags["name"], "Trolebus Quitumbe => La Y");
+    assert_eq!(pts[0].stops.iter().count(), 31);
+    assert_eq!(pts[1].id, 2030162);
+    assert_eq!(pts[1].tags["name"], "B6 Mapasingue Oeste Ida");
+    assert_eq!(pts[1].stops.iter().count(), 1);
 }
